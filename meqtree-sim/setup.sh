a3159a36cce1fb7f0b2202968c0845f0dc31a5c2
Piyanat Kittiwisit <pkittiwisit@sarao.ac.za>
2025-10-28 07:49:15 +0000

    Update scripts and configs

--- meqtree-sim/setup.sh
+++ meqtree-sim/setup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 # Setup Python environment
-conda create -n mkp python=3.12 pip
+conda create -y -n mkp python=3.12 pip
 conda activate mkp
-pip install stimela cult-cargo simms
+pip install stimela cult-cargo simms eidos casaconfig casadata casatasks casatools
 
-# Ensure that singularity exist
-# if not
-# $(which singularity)
-# raise error and exist
+# Ensure that casadata directory is populated
+mkdir -p $HOME/.casa/dat
+
+# Ensure that singularity/apptainer build directory is set to $HOME/tmp
+# and the directory is populated as users usually do not have disk quota 
+# to the the default /tmp 
+mkdir -p $HOME/tmp
+
+cat << 'MY_BASHRC_BLOCK' >> ~/.bashrc
+
+# Move singularity/apptainer build directory from the default /tmp"
+export SINGULARITY_TMPDIR=$HOME/tmp
+MY_BASHRC_BLOCK
+source ~/.bashrc
 
 # Build singularity image for meqtree-pipeliner
 # The image will be stored in ~/.singularity/
