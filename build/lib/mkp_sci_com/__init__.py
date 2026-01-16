a0383d19b62cadad916979d5e498dd8032ce48aa
Piyanat Kittiwisit <pkittiwisit@sarao.ac.za>
2026-01-16 09:41:21 +0000

    Move split_beam.py to mkp_sci_com.utils and add utils package

---
+++ build/lib/mkp_sci_com/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for mkp_sci_com."""
+# The package exposes subpackages directly. Users can import the simulation
+# helpers with `from mkp_sci_com import sim`.
+from . import sim  # noqa: F401
+
+__all__ = ["sim"]
