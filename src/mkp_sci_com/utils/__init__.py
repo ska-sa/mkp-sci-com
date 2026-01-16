ab55eab39de00a805758d71e8f151b53b0c81305
Piyanat Kittiwisit <pkittiwisit@sarao.ac.za>
2026-01-16 09:53:24 +0000

    Remove old utils package directory (migrated to single module)

--- src/mkp_sci_com/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Utility modules for mkp_sci_com.
-
-This package provides small helpers used across the repository. The
-`split_beam` CLI is available as the submodule `mkp_sci_com.utils.split_beam`.
-"""
-from . import split_beam  # noqa: F401
-
-__all__ = ["split_beam"]
