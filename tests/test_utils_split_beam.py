11093798fdabff256a4de39fa72079c25fc6ab04
Piyanat Kittiwisit <pkittiwisit@sarao.ac.za>
2026-01-16 09:53:09 +0000

    Expose split_beam as function in single module ; update Stimela cab to call python function

--- tests/test_utils_split_beam.py
+++ tests/test_utils_split_beam.py
@@ -1,8 +1,3 @@
 def test_import_split_beam():
-    import importlib
-    # import the utils package and the module
-    utils = importlib.import_module('mkp_sci_com.utils')
-    assert hasattr(utils, 'split_beam')
-    mod = importlib.import_module('mkp_sci_com.utils.split_beam')
-    assert hasattr(mod, 'split_beam')
-    assert callable(mod.split_beam)
+    from mkp_sci_com.utils import split_beam
+    assert callable(split_beam)
